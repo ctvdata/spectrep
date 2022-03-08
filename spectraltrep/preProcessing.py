5c101fa333a1d4108894ef5c6a79fab3e31303a4
Melesio Crespo-Sanchez <mel.crespo18@gmail.com>
2022-03-08 11:33:47 -0600

    Se implementaron las clases Lexic, Syntactic y Semantic Preprocessor

--- spectraltrep/preProcessing.py
+++ spectraltrep/preProcessing.py
@@ -80,6 +80,7 @@ class LexicPreprocessor(Preprocessor, Thread):
         self.__DELETE_PUNCTUATION = re.compile('[^\w\s]') # Eliminacion de signos de puntuacion
         self.__REPLACE_DIGITS = re.compile('\d') # Reemplazo de digitos
         self.__stop_words = set(stopwords.words('english'))
+        self.__wordnet_lemmatizer = WordNetLemmatizer()
     
     def __preProcess(self, text):
         text = text.lower()
@@ -94,9 +95,7 @@ class LexicPreprocessor(Preprocessor, Thread):
         
         word_tokens = word_tokenize(text)
         text = [w for w in word_tokens if not w in self.__stop_words]
-        
-        wordnet_lemmatizer = WordNetLemmatizer()    
-        text = [wordnet_lemmatizer.lemmatize(w) for w in text]
+        text = [self.__wordnet_lemmatizer.lemmatize(w) for w in text]
         
         text = ' '.join(text)    
         
@@ -106,33 +105,112 @@ class LexicPreprocessor(Preprocessor, Thread):
         while(True):
             batch = self.__dispatcher.getBatch()
             if(batch != '<EOC>'):
-                batchId = batch[0]
-                documents = batch[1]
-                documents = [self.__preProcess(t) for t in documents]
+                documents = [self.__preProcess(t) for t in batch[1]]
 
-                self.__sink.addPreprocessedBatch(self, (batchId, documents))
+                self.__sink.addPreprocessedBatch(self, (batch[0], documents))
             else:
                 break
 
 class SyntacticPreprocessor(Preprocessor):
-    def __init__(self):
-        Thread.__init__(self)
+    def __init__(self, dispatcher, sink):
+        try:
+            if isinstance(dispatcher, Dispatcher):
+                self.__dispatcher = dispatcher
+            else:
+                raise Exception("Non-valid instance of dispatcher.")
 
+            if isinstance(sink, Sink):
+                self.__sink = sink
+            else:
+                raise Exception("Non-valid instance of Sink.")
+        
+        except Exception as err:
+            print(err)
+
+        Thread.__init__(self)        
+        self.__DELETE_NEW_LINE = re.compile('\\n') # Reemplazo de saltos de linea
+        self.__DELETE_MIDSCORE = re.compile('-') # Eliminacion de guion medio
+        self.__DELETE_PARENTHESES = re.compile('\(|\)') # Eliminación de paréntesis
+        self.__DELETE_BRACKETS = re.compile('\[|\]') # Eliminacion de corchetes
+        self.__REPLACE_DOUBLE_SPACE = re.compile('\s+') # Remplazo de espacios dobles
+        self.__DELETE_QM = re.compile('"|’|\'') # Eliminar comillas
+        self.__DELETE_PUNCTUATION = re.compile('[^\w\s]') # Eliminacion de signos de puntuacion
+        self.__REPLACE_DIGITS = re.compile('\d') # Reemplazo de digitos
+    
     def __preProcess(self, text):
-        pass
+        text = text.lower()
+        text = self.__DELETE_NEW_LINE.sub("", text)
+        text = self.__DELETE_MIDSCORE.sub(" ", text)
+        text = self.__DELETE_PARENTHESES.sub("", text)    
+        text = self.__DELETE_BRACKETS.sub("", text)    
+        text = self.__REPLACE_DOUBLE_SPACE.sub(" ", text)   
+        text = self.__DELETE_QM.sub("", text)
+        text = self.__DELETE_PUNCTUATION.sub('', text)
+        text = self.__REPLACE_DIGITS.sub('<NUM>', text)
+        text = word_tokenize(text)
+        text = nltk.pos_tag(text)
+        text = ' '.join(text)    
+        
+        return(text)
 
     def run(self):
-        pass
+        while(True):
+            batch = self.__dispatcher.getBatch()
+            if(batch != '<EOC>'):
+                documents = [self.__preProcess(t) for t in batch[1]]
+
+                self.__sink.addPreprocessedBatch(self, (batch[0], documents))
+            else:
+                break
 
 class SemanticPreprocessor(Preprocessor, Thread):
-    def __init__(self):
-        Thread.__init__(self)
+    def __init__(self, dispatcher, sink):
+        try:
+            if isinstance(dispatcher, Dispatcher):
+                self.__dispatcher = dispatcher
+            else:
+                raise Exception("Non-valid instance of dispatcher.")
 
+            if isinstance(sink, Sink):
+                self.__sink = sink
+            else:
+                raise Exception("Non-valid instance of Sink.")
+        
+        except Exception as err:
+            print(err)
+
+        Thread.__init__(self)        
+        self.__DELETE_NEW_LINE = re.compile('\\n') # Reemplazo de saltos de linea
+        self.__DELETE_MIDSCORE = re.compile('-') # Eliminacion de guion medio
+        self.__DELETE_PARENTHESES = re.compile('\(|\)') # Eliminación de paréntesis
+        self.__DELETE_BRACKETS = re.compile('\[|\]') # Eliminacion de corchetes
+        self.__REPLACE_DOUBLE_SPACE = re.compile('\s+') # Remplazo de espacios dobles
+        self.__DELETE_QM = re.compile('"|’|\'') # Eliminar comillas
+        self.__DELETE_PUNCTUATION = re.compile('[^\w\s]') # Eliminacion de signos de puntuacion
+        self.__REPLACE_DIGITS = re.compile('\d') # Reemplazo de digitos
+    
     def __preProcess(self, text):
-        pass
+        text = text.lower()
+        text = self.__DELETE_NEW_LINE.sub("", text)
+        text = self.__DELETE_MIDSCORE.sub(" ", text)
+        text = self.__DELETE_PARENTHESES.sub("", text)    
+        text = self.__DELETE_BRACKETS.sub("", text)    
+        text = self.__REPLACE_DOUBLE_SPACE.sub(" ", text)   
+        text = self.__DELETE_QM.sub("", text)
+        text = self.__DELETE_PUNCTUATION.sub('', text)
+        text = self.__REPLACE_DIGITS.sub('<NUM>', text)
+        
+        return(text)
 
     def run(self):
-        pass
+        while(True):
+            batch = self.__dispatcher.getBatch()
+            if(batch != '<EOC>'):
+                documents = [self.__preProcess(t) for t in batch[1]]
+
+                self.__sink.addPreprocessedBatch(self, (batch[0], documents))
+            else:
+                break
 
 class PreprocessorAbstractFactory(metaclass=ABCMeta):
     @abstractmethod
