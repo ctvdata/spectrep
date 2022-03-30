fd5cff32b52d8f1e74571d6dd9d9605a8d7f1fa3
Melesio Crespo-Sanchez <mel.crespo18@gmail.com>
2022-03-30 09:31:55 -0600

    se implementaron los metodos fit y transform de la clase Projector/modulo spaceUnification

--- spectraltrep/spaceUnification.py
+++ spectraltrep/spaceUnification.py
@@ -1,5 +1,9 @@
 from abc import ABCMeta
 from abc import abstractmethod
+import numpy as np
+import sys
+sys.path.append('..')
+from simpsom import SOMNet
 
 class Reader(metaclass=ABCMeta):
     @abstractmethod
@@ -11,22 +15,42 @@ class CorpusReader(Reader):
         pass
 
 class Projector:
-    def __init__(self):
-        self.__somModel = None
-    
-    @property
-    def somModel(self):
-        return self.__somModel
+    def __init__(self, netLength=20, learningRate=0.01, epochs=1000):
+        self.netLength=netLength
+        self.learningRate=learningRate
+        self.epochs=epochs
 
-    @somModel.setter
-    def somModel(self, model):
-        self.__somModel = model
+    def fit(self, featureVectors):
+        self.net = SOMNet(self.netLength, self.netLength, featureVectors, PBC=True)
+        self.net.train(self.learningRate, self.epochs)
+
+    def getProjection(self, featureVectors):
+        """
+        X: Matrix to project over the som grid of size (num of vectors, num of features)
+        nodeOperation: 'dot': Dot product or 'invEuc': inverse of euclidean distance
+        """
+        nrows = featureVectors.shape[0]
+        i=0
+        result_matrix = []
+        for row in np.array(featureVectors): 
+            j=0
+            image_document=np.zeros(shape=self.netLength*self.netLength, dtype=np.float32)
+            for node in self.net.nodeList:
+                image_document[j] = 1/np.linalg.norm(row-node.weights)
+                j=j+1
+            mn=image_document.min()
+            mx=image_document.max()
+            image_document = (255.9 * (image_document-mn)/(mx-mn)).astype(np.uint8)
+            image_document=image_document.reshape(self.netLength,self.netLength)
+            result_matrix.append(image_document)  
+            s = "{}% Complete".format(int((i*100)/nrows))
+            print(s, end='\r')
+            i=i+1
+        print("100% Complete", end='\r')
+        return result_matrix 
 
     def saveSomModel(self, outputPath):
         pass
 
-    def fit(self, featureVectors):
-        pass
-
-    def getProjection(self, featureVectors):
+    def loadSomModel(self, inputPath):
         pass
---
+++ tests/Projector.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append('..')
+from simpsom import SOMNet
+import numpy as np
+import time
+
+init = time.time()
+SOMobj = SOMNet(10, 10, np.random.random(50000000).reshape(5000,10000), PBC=True)
+
+SOMobj.train(epochs=10, start_learning_rate=.01)
+print("Entrenamiento finalizado en %.02f " % (time.time()-init))
